6a122eac37fb46a9d53efda5349a4295a22010fe
Mastagoon <inzilagiway@gmail.com>
2021-11-23 23:13:52 +0300

    first commit

---
+++ pages/news.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const News: NextPage = () => {
+    return (
+        <h1>
+            news page
+        </h1>
+    )
+}
+
+export default News
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Test: NextPage = () => {
+    return (
+        <div>
+           hello world 
+        </div>
+    )
+}
+
+export default Test
